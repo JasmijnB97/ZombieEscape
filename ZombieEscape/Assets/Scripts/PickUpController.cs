04dc69bb9277dd44d7204d28e0484a9e13901a53
R0vi <ivorstaatsl0l@gmail.com>
2020-10-09 10:23:32 +0200

    added player and pickup

---
+++ ZombieEscape/Assets/Scripts/PickUpController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    public GameObject pickupObject;
+
+    private bool pickedUp = false;
+
+    void Update()
+    {
+        if (pickedUp)
+        {
+            pickupObject.transform.position = transform.position + transform.forward * 0.6f + transform.up * 0.2f + transform.right * 0.4f;
+            pickupObject.transform.rotation = transform.rotation;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(pickupObject))
+            pickedUp = true;
+    }
+}
