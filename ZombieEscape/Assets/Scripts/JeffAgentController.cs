f87178dad21774b9c406758506711147c4a315b6
R0vi <ivorstaatsl0l@gmail.com>
2020-10-09 15:49:17 +0200

    maken

--- ZombieEscape/Assets/Scripts/JeffAgentController.cs
+++ ZombieEscape/Assets/Scripts/JeffAgentController.cs
@@ -25,11 +25,6 @@ public class JeffAgentController : MonoBehaviour
         {
             animator.SetBool("jeffIsWalking", true);
             agent.SetDestination(player.transform.position);            
-        } if(playerDistance >= teleportRadius)
-        {
-            agent.transform.position = new Vector3(player.transform.position.x + 10f, transform.position.y, player.transform.position.z + 10f);
-            animator.SetBool("jeffIsWalking", true);
-            agent.SetDestination(player.transform.position);
         }
         else
         {
--- ZombieEscape/Assets/Scripts/NielsAgentController.cs
+++ ZombieEscape/Assets/Scripts/NielsAgentController.cs
@@ -26,12 +26,6 @@ public class NielsAgentController : MonoBehaviour
             animator.SetBool("nielsIsWalking", true);
             agent.SetDestination(player.transform.position);
         }
-        if (playerDistance >= teleportRadius)
-        {
-            agent.transform.position = new Vector3(player.transform.position.x + 10f, transform.position.y, player.transform.position.z + 10f);
-            animator.SetBool("nielsIsWalking", true);
-            agent.SetDestination(player.transform.position);
-        }
         else
         {
             animator.SetBool("nielsIsWalking", false);
