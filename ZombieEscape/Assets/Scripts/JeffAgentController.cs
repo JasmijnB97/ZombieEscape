04d2b56e50c2747c56fa0a853363e3ebd3507b1b
Jasmijn Bartelds <jasmijn.bartelds@hotmail.nl>
2020-10-09 11:23:47 +0200

    Added animated navmesh agent zombies

---
+++ ZombieEscape/Assets/Scripts/JeffAgentController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class JeffAgentController : MonoBehaviour
+{
+    static Animator animator;
+    public NavMeshAgent agent;
+    public float seePlayerRadius;
+    private GameObject player;
+    private float playerDistance;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        playerDistance = Vector3.Distance(player.transform.position, transform.position);
+        if (playerDistance <= seePlayerRadius)
+        {
+            animator.SetBool("jeffIsWalking", true);
+            agent.SetDestination(player.transform.position);            
+        } else
+        {
+            animator.SetBool("jeffIsWalking", false);
+        }
+    }
+}
