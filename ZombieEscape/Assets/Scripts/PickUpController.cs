9ecccd8f4732dc7750fb9f267d6cc29d48542e49
R0vi <ivorstaatsl0l@gmail.com>
2020-10-09 14:58:59 +0200

    added saving

--- ZombieEscape/Assets/Scripts/PickUpController.cs
+++ ZombieEscape/Assets/Scripts/PickUpController.cs
@@ -8,10 +8,12 @@ public class PickUpController : MonoBehaviour
     public GameObject pickupObject;
     public float startYPositionPickupObject;
     public Text DeathText;
+    public Text SaveText;
 
     private bool pickedUp = false;
     private Vector3 respawnPosition;
     private Quaternion respawnRotation;
+    private int saveCount = 0;
 
     void Start()
     {
@@ -19,13 +21,14 @@ public class PickUpController : MonoBehaviour
         respawnRotation = transform.rotation;
         DeathText.text = "Death";
         DeathText.gameObject.SetActive(false);
+        SaveText.gameObject.SetActive(false);
     }
 
     void Update()
     {
         if (pickedUp)
         {
-            pickupObject.transform.position = transform.position + transform.forward * 0.6f + transform.up * 0.2f + transform.right * 0.4f;
+            pickupObject.transform.position = transform.position + transform.forward * 0.8f + transform.up * 0.2f + transform.right * 0.4f;
             pickupObject.transform.rotation = transform.rotation;
         }
     }
@@ -37,12 +40,32 @@ public class PickUpController : MonoBehaviour
 
         if (collision.transform.CompareTag("Zombie"))
         {
-            DeathText.gameObject.SetActive(true);
-            DropItem();
-            Respawn();
-            new WaitForSeconds(5);
-            DeathText.gameObject.SetActive(false);
+            StartCoroutine(HitByZombie());
         }
+
+        if (collision.transform.CompareTag("Student") && pickedUp)
+        {
+            collision.gameObject.SetActive(false);
+            StartCoroutine(SaveStudentText());
+        }
+    }
+
+    IEnumerator HitByZombie()
+    {
+        DeathText.gameObject.SetActive(true);
+        DropItem();
+        Respawn();
+        yield return new WaitForSeconds(2.5f);
+        DeathText.gameObject.SetActive(false);
+    }
+
+    IEnumerator SaveStudentText()
+    {
+        saveCount++;
+        SaveText.text = saveCount + "/3 students saved";
+        SaveText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        SaveText.gameObject.SetActive(false);
     }
 
     private void DropItem()
